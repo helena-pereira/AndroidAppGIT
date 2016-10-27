9c2806a2548f053ffcd541fa70a8a32f1fa96b4a
Helena <helena.pereira@students.hevs.ch>
2016-10-27 17:07:04 +0200

    Helena

--- app/src/main/java/com/example/helena/myapplication/MainActivity.java
+++ app/src/main/java/com/example/helena/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helena.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.accueil,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
